aa6e56f9a0828495104f1b27a6a6e58e866e087d
Ilari Iso-Junno <ilari.isojunno@gmail.com>
2023-02-12 16:33:55 +0200

    day 3 part 2

--- day_3/main.py
+++ day_3/main.py
@@ -7,14 +7,32 @@ def split_into_two(input_string):
     return input_string[:half], input_string[-half:]
 
 
-def rucksacks(file_name):
+def raw_data(file_name):
     with open(file_name) as file:
         for line in file.readlines():
-            yield split_into_two(line.strip())
+            yield line.strip()
+
+
+def rucksacks(file_name):
+    for line in raw_data(file_name):
+        yield split_into_two(line)
+
+
+def group_bags(file_name):
+    iterator = iter(raw_data(file_name))
+    while True:
+        try:
+            yield next(iterator), next(iterator), next(iterator)
+        except StopIteration:
+            break
 
 
 def priority(item):
     return string.ascii_letters.index(item) + 1
+
+
+def sum_of_priorities(items):
+    return sum(priority(item) for item in items)
 
 
 def main():
@@ -23,7 +41,13 @@ def main():
         set(compartment_a).intersection(set(compartment_b)).pop()
         for compartment_a, compartment_b in rucksacks(file_name)
     ]
-    print(sum((priority(item) for item in common_items)))
+    print(sum_of_priorities(common_items))
+
+    common_badges = [
+        set(bag_a).intersection(bag_b).intersection(bag_c).pop()
+        for bag_a, bag_b, bag_c in group_bags(file_name)
+    ]
+    print(sum_of_priorities(common_badges))
 
 
 if __name__ == '__main__':
