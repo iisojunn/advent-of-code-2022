7e31d34a5c9e67cf6dfd42d0a936ad9a9cfa9c3f
Ilari Iso-Junno <ilari.isojunno@gmail.com>
2023-02-16 18:54:30 +0200

    day 4 part 1

---
+++ day_4/main.py
@@ -0,0 +1,34 @@
+import sys
+
+
+class Range:
+    def __init__(self, string):
+        lower, higher = string.split("-")
+        self.lower = int(lower)
+        self.higher = int(higher)
+
+    def __repr__(self):
+        return f"{self.lower}-{self.higher}"
+
+    def contains(self, other):
+        return self.lower <= other.lower and other.higher <= self.higher
+
+
+def section_ranges(file_name):
+    with open(file_name) as file:
+        for line in file.readlines():
+            first, second = line.strip().split(",")
+            yield Range(first), Range(second)
+
+
+def main():
+    file_name = sys.argv[1]
+    contains_fully = 0
+    for range_a, range_b in section_ranges(file_name):
+        if range_a.contains(range_b) or range_b.contains(range_a):
+            contains_fully += 1
+    print(contains_fully)
+
+
+if __name__ == '__main__':
+    main()
