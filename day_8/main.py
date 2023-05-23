2cbf26587aad17ae1481ca09f8bf9526bbc2774c
Ilari Iso-Junno <ilari.isojunno@gmail.com>
2023-05-24 00:44:06 +0300

    day 8 part 2

--- day_8/main.py
+++ day_8/main.py
@@ -1,4 +1,6 @@
+import operator
 import sys
+from functools import reduce
 
 
 def read_input(filename):
@@ -7,7 +9,7 @@ def read_input(filename):
 
 
 def left_side(tree_map, x, y):
-    for _x in range(0, x):
+    for _x in range(x - 1, -1, -1):
         yield tree_map[_x][y]
 
 
@@ -17,7 +19,7 @@ def right_side(tree_map, x, y):
 
 
 def up_side(tree_map, x, y):
-    for _y in range(0, y):
+    for _y in range(y - 1, -1, -1):
         yield tree_map[x][_y]
 
 
@@ -33,9 +35,13 @@ def is_visible_from(side, tree_map, x, y):
     return tree_map[x][y] > max(tree_lengths)
 
 
+def all_directions():
+    return left_side, up_side, right_side, down_side
+
+
 def is_visible(tree_map, x, y):
-    for side in (left_side, up_side, right_side, down_side):
-        if is_visible_from(side, tree_map, x, y):
+    for direction in all_directions():
+        if is_visible_from(direction, tree_map, x, y):
             return True
     return False
 
@@ -47,10 +53,36 @@ def visible_trees(tree_map):
                 yield x, y
 
 
+def visibility_to_one_direction(direction, tree_map, x, y):
+    height = tree_map[x][y]
+    visibility = 0
+    for tree in direction(tree_map, x, y):
+        visibility += 1
+        if tree >= height:
+            break
+    return visibility
+
+
+def visibility_to_all_directions(tree_map, x, y):
+    for direction in all_directions():
+        yield visibility_to_one_direction(direction, tree_map, x, y)
+
+
+def single_scenic_score(tree_map, x, y):
+    return reduce(operator.mul, visibility_to_all_directions(tree_map, x, y))
+
+
+def scenic_scores(tree_map):
+    for x in range(len(tree_map)):
+        for y in range(len(tree_map[0])):
+            yield single_scenic_score(tree_map, x, y)
+
+
 def main():
     filename = sys.argv[1]
     tree_map = read_input(filename)
     print(len(tuple(visible_trees(tree_map))))
+    print(max(scenic_scores(tree_map)))
 
 
 if __name__ == '__main__':
