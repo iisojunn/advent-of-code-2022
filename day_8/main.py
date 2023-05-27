a05129ebfa7bb488782e783ce934f9d98b23f834
Ilari Iso-Junno <ilari.isojunno@gmail.com>
2023-05-27 14:17:07 +0300

    refactor day8

--- day_8/main.py
+++ day_8/main.py
@@ -1,6 +1,7 @@
 import operator
 import sys
 from functools import reduce
+from itertools import product
 
 
 def read_input(filename):
@@ -8,8 +9,13 @@ def read_input(filename):
         return [line.strip() for line in file.readlines()]
 
 
+def to_zero(start):
+    for value in range(start - 1, -1, -1):
+        yield value
+
+
 def left_side(tree_map, x, y):
-    for _x in range(x - 1, -1, -1):
+    for _x in to_zero(x):
         yield tree_map[_x][y]
 
 
@@ -19,7 +25,7 @@ def right_side(tree_map, x, y):
 
 
 def up_side(tree_map, x, y):
-    for _y in range(y - 1, -1, -1):
+    for _y in to_zero(y):
         yield tree_map[x][_y]
 
 
@@ -28,11 +34,12 @@ def down_side(tree_map, x, y):
         yield tree_map[x][_y]
 
 
-def is_visible_from(side, tree_map, x, y):
-    tree_lengths = tuple(side(tree_map, x, y))
-    if not tree_lengths:
+def is_visible_behind(trees, tree_height):
+    try:
+        tallest_tree = max(trees)
+    except ValueError:
         return True
-    return tree_map[x][y] > max(tree_lengths)
+    return tree_height > tallest_tree
 
 
 def all_directions():
@@ -40,32 +47,33 @@ def all_directions():
 
 
 def is_visible(tree_map, x, y):
-    for direction in all_directions():
-        if is_visible_from(direction, tree_map, x, y):
-            return True
-    return False
+    tree_height = tree_map[x][y]
+    return any(is_visible_behind(direction(tree_map, x, y), tree_height) for direction in all_directions())
 
 
 def visible_trees(tree_map):
-    for x in range(len(tree_map)):
-        for y in range(len(tree_map[0])):
-            if is_visible(tree_map, x, y):
-                yield x, y
+    for x, y in all_coordinates(tree_map):
+        if is_visible(tree_map, x, y):
+            yield x, y
 
 
-def visibility_to_one_direction(direction, tree_map, x, y):
-    height = tree_map[x][y]
+def all_coordinates(tree_map):
+    return product(range(len(tree_map)), range(len(tree_map[0])))
+
+
+def visibility_to_one_direction(trees, blocking_height):
     visibility = 0
-    for tree in direction(tree_map, x, y):
+    for tree_height in trees:
         visibility += 1
-        if tree >= height:
+        if tree_height >= blocking_height:
             break
     return visibility
 
 
 def visibility_to_all_directions(tree_map, x, y):
+    height = tree_map[x][y]
     for direction in all_directions():
-        yield visibility_to_one_direction(direction, tree_map, x, y)
+        yield visibility_to_one_direction(direction(tree_map, x, y), height)
 
 
 def single_scenic_score(tree_map, x, y):
@@ -73,9 +81,8 @@ def single_scenic_score(tree_map, x, y):
 
 
 def scenic_scores(tree_map):
-    for x in range(len(tree_map)):
-        for y in range(len(tree_map[0])):
-            yield single_scenic_score(tree_map, x, y)
+    for x, y in all_coordinates(tree_map):
+        yield single_scenic_score(tree_map, x, y)
 
 
 def main():
